b1f7e4912f7fd94b38a7d72474dbe7161e1422cd
Ruslan Ry <ruslan.ry@gmail.com>
2018-02-02 19:49:57 +0300

    Initial commit

---
+++ src/PhoneDB.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneDB {
+    public static void main(String[] args){
+        HashMap<String,ArrayList> phoneDB = getPhoneDB();
+
+    }
+
+    public static HashMap<String,ArrayList>  getPhoneDB(){
+        return new HashMap<String,ArrayList>();
+    }
+}
